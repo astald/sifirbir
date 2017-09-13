1c26a7de2b14f97d4bb24922d59b4024c4723e6f
osman <info@astald.com>
2017-09-13 20:25:05 +0300

    a few new updates

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
     <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/sifirbir.css">
     <title>Sıfır Bir</title>
 </head>
@@ -67,16 +68,16 @@
             <h2>Sıfır Bir</h2>
         </div>
         <div class="row">
-            <div class="col-sm-6">
+            <div class="col-md-6">
                 <img src="assets/img/gallery/s1.jpg" alt="" class="img-fluid">
             </div>
-            <div class="col-sm-6">
+            <div class="col-md-6">
                 <p>Savaş, Cio ve Özgür, Adana'nın Hürriyet Mahallesi'nde doğup büyümüş, 20 li yaşlarında 3 gençtir.</p>
                 <p>Savaş hem yaşı itibariyle, daha da önemlisi cesareti, gözükaralığı, zekası ve kiritik zamanlarda aldığı doğru kararlar nedeniyle ''Abi'' konumundadır. Zamanla bu özelliği sayesinde kendisinden yaşça büyüklerin bile 'Abisi' olacaktır.</p>
                 <p>Cio ise biraz gösteriş meraklısı, gücüyle ve yaptıklarıyla övünmeyi seven, ama bunları bile hep planlı şekilde yapan birisidir.</p>
             </div>
         </div>
-        <div class="text-center"><a href="#" class="continue">Devamını Görüntüle</a></div>
+        <div class="text-center"><a href="#" class="btn read-more ">Devamını Görüntüle</a></div>
     </div>
     <div class="seasons home-block">
         <div class="heading-block text-center">
@@ -84,40 +85,58 @@
             <h2>Tüm Bölümler</h2>
         </div>
         <div class="row">
-            <div class="col-sm-4">
-                <a class="season">
+            <div class="col-sm-6 col-md-4">
+                <a href="#" class="season">
+                    <div class="image">
                     <img src="https://i.ytimg.com/vi/oJ29nRfKfNY/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDgcUM0B_q28Mkcmqt08TXxHy3HEg" alt="Season">
-                    <h4 class="title">Sıfır Bir - 1. Bölüm</h4>
+                    </div>
+                    <h5 class="title">Sıfır Bir - 1. Bölüm</h5>
+                    <small class="text-muted"><i class="fa fa-comments"></i> 5 yorum <i class="fa fa-clock-o"></i> 5 izlenme</small>
                 </a>
             </div>
-            <div class="col-sm-4">
-                <a class="season">
+            <div class="col-sm-6 col-md-4">
+                <a href="#" class="season">
+                    <div class="image">
                     <img src="https://i.ytimg.com/vi/egzlpBiCeoo/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA2frx-Llf2YULWRY0diIxYY0kPdw" alt="Season">
-                    <h4 class="title">Sıfır Bir - 2. Bölüm</h4>
+                    </div>
+                    <h5 class="title">Sıfır Bir - 2. Bölüm</h5>
+                    <small class="text-muted"><i class="fa fa-comments"></i> 5 yorum <i class="fa fa-clock-o"></i> 5 izlenme</small>
                 </a>
             </div>
-            <div class="col-sm-4">
-                <a class="season">
+            <div class="col-sm-6 col-md-4">
+                <a href="#" class="season">
+                    <div class="image">
                     <img src="https://i.ytimg.com/vi/I5rP5hb_c5E/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBC2s-wBRWOHookeiOahxhwjhJCpQ" alt="Season">
-                    <h4 class="title">Sıfır Bir - 3. Bölüm</h4>
+                    </div>
+                    <h5 class="title">Sıfır Bir - 3. Bölüm</h5>
+                    <small class="text-muted"><i class="fa fa-comments"></i> 5 yorum <i class="fa fa-clock-o"></i> 5 izlenme</small>
                 </a>
             </div>
-            <div class="col-sm-4">
-                <a class="season">
+            <div class="col-sm-6 col-md-4">
+                <a href="#" class="season">
+                    <div class="image">
                     <img src="https://i.ytimg.com/vi/H_6hKRmpxiE/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBOOeIK_zll8n5I-J_-7oW9Z2EKgg" alt="Season">
-                    <h4 class="title">Sıfır Bir - 4. Bölüm</h4>
+                    </div>
+                    <h5 class="title">Sıfır Bir - 4. Bölüm</h5>
+                    <small class="text-muted"><i class="fa fa-comments"></i> 5 yorum <i class="fa fa-clock-o"></i> 5 izlenme</small>
                 </a>
             </div>
-            <div class="col-sm-4">
-                <a class="season">
+            <div class="col-sm-6 col-md-4">
+                <a href="#" class="season">
+                    <div class="image">
                     <img src="https://i.ytimg.com/vi/sliXwKUgHfs/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDIrV5IO1wwTOZSZ9nSEmRG1vWS5Q" alt="Season">
-                    <h4 class="title">Sıfır Bir - 6. Bölüm</h4>
+                    </div>
+                    <h5 class="title">Sıfır Bir - 6. Bölüm</h5>
+                    <small class="text-muted"><i class="fa fa-comments"></i> 5 yorum <i class="fa fa-clock-o"></i> 5 izlenme</small>
                 </a>
             </div>
-            <div class="col-sm-4">
-                <a class="season">
+            <div class="col-sm-6 col-md-4">
+                <a href="#" class="season">
+                    <div class="image">
                     <img src="https://i.ytimg.com/vi/sDj_nL7qFh4/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBnOt8UpkMLxeVZSy_tATgOrbmtpA" alt="Season">
-                    <h4 class="title">Sıfır Bir - 5. Bölüm</h4>
+                    </div>
+                    <h5 class="title">Sıfır Bir - 5. Bölüm</h5>
+                    <small class="text-muted"><i class="fa fa-comments"></i> 5 yorum <i class="fa fa-clock-o"></i> 5 izlenme</small>
                 </a>
             </div>
         </div>
@@ -176,10 +195,62 @@
     </div>
 
 </section>
-<footer>
-    <div class="copyright container">
-        &copy; Sıfır Bir 2017 Tüm Hakları Saklıdır.
+<footer class="footer">
+<div class="container">
+    <div class="row row-p b-md">
+        <div class="col-md-3 col-sm-6">
+            <div class="widget">
+                <h5>Sıfır Bir</h5>
+                <ul class="footer-links">
+                    <li><a href="index.html">Ana Sayfa <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Bölümler</a></li>
+                    <li><a href="#">Fragmanlar</a></li>
+                    <li><a href="#">Teaserler</a></li>
+                    <li><a href="#">Yazılar</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="widget">
+                <h5>Bağlantılar</h5>
+                <ul class="footer-links">
+                    <li><a href="#">Kamera Arkası</a></li>
+                    <li><a hreflang="tr-tr" href="#/firma-basvuru">Oyuncu Başvurusu</a></li>
+                    <li><a hreflang="tr-tr" href="#/sayfa/gizlilik-bildirimi">Gizlilik Bildirimi</a></li>
+                    <li><a hreflang="tr-tr" href="#">Hakkımızda</a></li>
+                    <li><a hreflang="tr-tr" href="#">İletişim</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-3 col-sm-6">
+            <div class="widget">
+                <h5>İletişim</h5>
+                <ul class="footer-links">
+                    <li><a hreflang="tr-tr" href="#/iletisim"><i class="fa fa-file-o"></i> İletişim Formu</a></li>
+                    <li><a hreflang="tr-tr" href="mailto:info@sifirbir.tv"><i class="fa fa-envelope-o"></i> info@sifirbir.tv</a></li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="widget">
+                <h5>Sosyal Medya</h5>
+                <span class="social-icons">
+                    <a hreflang="tr-tr" href="https://www.facebook.com/sifirbir" class="fa fa-facebook" target="_blank" rel="nofollow"></a>
+                    <a hreflang="tr-tr" href="https://www.twitter.com/sifirbir" class="fa fa-twitter" target="_blank" rel="nofollow"></a>
+                    <a hreflang="tr-tr" href="https://www.instagram.com/sifirbir" class="fa fa-instagram" target="_blank" rel="nofollow"></a>
+                    <a hreflang="tr-tr" href="https://tr.linkedin.com/company/#" class="fa fa-linkedin" target="_blank" rel="nofollow"></a>
+                </span>
+            </div>
+        </div>
     </div>
+        <div class="copyright">
+            &copy; Sıfır Bir 2017 Tüm Hakları Saklıdır.
+        </div>
+
+</div>
 </footer>
 
 <script src="assets/plugins/jquery/js/jquery.min.js"></script>
