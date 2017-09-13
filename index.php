96aef8095edf960c83f69aba53dfd8d205275f8a
osman <osman@hmdsa.com>
2017-09-13 15:26:59 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/sifirbir.css">
+    <title>Sıfır Bir</title>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+        <div class="container">
+        <a class="navbar-brand" href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSifirBir" aria-controls="navbarSifirBir" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSifirBir">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a class="nav-link" href="index.html">ANA SAYFA <span class="sr-only">(current)</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">SEZONLAR</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">HAKKIMIZDA</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">SPONSORLAR</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">İLETİŞİM</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Content here -->
+</header>
+<section class="container">
+    <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="https://i.ytimg.com/vi/oJ29nRfKfNY/maxresdefault.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://i.ytimg.com/vi/TgnGAXGCcew/maxresdefault.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://i.ytimg.com/vi/M5FHvTX93W8/maxresdefault.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="about-us home-block">
+        <div class="heading-block text-center">
+            <h6>Hakkında</h6>
+            <h2>Sıfır Bir</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="assets/img/gallery/s1.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-sm-6">
+                <p>Savaş, Cio ve Özgür, Adana'nın Hürriyet Mahallesi'nde doğup büyümüş, 20 li yaşlarında 3 gençtir.</p>
+                <p>Savaş hem yaşı itibariyle, daha da önemlisi cesareti, gözükaralığı, zekası ve kiritik zamanlarda aldığı doğru kararlar nedeniyle ''Abi'' konumundadır. Zamanla bu özelliği sayesinde kendisinden yaşça büyüklerin bile 'Abisi' olacaktır.</p>
+                <p>Cio ise biraz gösteriş meraklısı, gücüyle ve yaptıklarıyla övünmeyi seven, ama bunları bile hep planlı şekilde yapan birisidir.</p>
+                <a href="#">Devamını Görüntüle</a>
+            </div>
+        </div>
+    </div>
+    <div class="seasons home-block">
+        <div class="heading-block text-center">
+            <h6>Dizi</h6>
+            <h2>Tüm Bölümler</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <a class="season">
+                    <img src="https://i.ytimg.com/vi/oJ29nRfKfNY/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDgcUM0B_q28Mkcmqt08TXxHy3HEg" alt="Season">
+                    <h4 class="title">Sıfır Bir - 1. Bölüm</h4>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a class="season">
+                    <img src="https://i.ytimg.com/vi/egzlpBiCeoo/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA2frx-Llf2YULWRY0diIxYY0kPdw" alt="Season">
+                    <h4 class="title">Sıfır Bir - 2. Bölüm</h4>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a class="season">
+                    <img src="https://i.ytimg.com/vi/I5rP5hb_c5E/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBC2s-wBRWOHookeiOahxhwjhJCpQ" alt="Season">
+                    <h4 class="title">Sıfır Bir - 3. Bölüm</h4>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a class="season">
+                    <img src="https://i.ytimg.com/vi/H_6hKRmpxiE/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBOOeIK_zll8n5I-J_-7oW9Z2EKgg" alt="Season">
+                    <h4 class="title">Sıfır Bir - 4. Bölüm</h4>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a class="season">
+                    <img src="https://i.ytimg.com/vi/sDj_nL7qFh4/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBnOt8UpkMLxeVZSy_tATgOrbmtpA" alt="Season">
+                    <h4 class="title">Sıfır Bir - 5. Bölüm</h4>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <a class="season">
+                    <img src="https://i.ytimg.com/vi/sliXwKUgHfs/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDIrV5IO1wwTOZSZ9nSEmRG1vWS5Q" alt="Season">
+                    <h4 class="title">Sıfır Bir - 6. Bölüm</h4>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="actors home-block">
+        <div class="heading-block text-center">
+            <h6>Kadro</h6>
+            <h2>Oyuncular</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-3">
+                <a href="#" class="actor">
+                    <img class="profile" src="assets/img/cadro/savas.jpg" alt="Card image cap">
+                    <div class="overlay"></div>
+                    <div class="information">
+                        <h4 class="name">Savaş Satış</h4>
+                        <p class="bio">“Savaş”. Grubun akıllı çocuğu. Plan yapan, arkadaşlarını yönlendiren, patron rolünü üstlenen arkadaş. Ha bir de “Allah’ını severim” var ki dillere pelesenk olur.</p>
+                        <small class="text-muted">Baş Aktör</small>
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <a href="#" class="actor">
+                    <img class="profile" src="assets/img/cadro/cio.jpg" alt="Card image cap">
+                    <div class="overlay"></div>
+                    <div class="information">
+                        <h4 class="name">Cihangir Ceyhan</h4>
+                        <p class="bio">“Cio”. Adam gibi adam bu Cio. Grubun silahı o. Tetiğe basan, öl dedim mi ölen, ama uğruna ölünecek adam. Kaba kuvvet arkadaşa ait. “Onlar da bizim gibi etten kemikten değil mi” dediyse Cio, birileri ölecek demektir.</p>
+                        <small class="text-muted">Baş Aktör, Patron</small>
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <a href="#" class="actor">
+                    <img class="profile" src="assets/img/cadro/ozgur.jpg" alt="Card image cap">
+                    <div class="overlay"></div>
+                    <div class="information">
+                        <h4 class="name">Özgür Meriç</h4>
+                        <p class="bio">“Özgür”. Grubun duygusal, ağır başlı adamı. Ara ara okuduğu/yazdığı şiirleri çok acayip. Arkadaşları için her şeyi yapıyor.</p>
+                        <small class="text-muted">Yedek Oyuncu</small>
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-3">
+                <a href="#" class="actor">
+                    <img class="profile" src="assets/img/cadro/anafor.jpg" alt="Card image cap">
+                    <div class="overlay"></div>
+                    <div class="information">
+                        <h4 class="name">Anafor</h4>
+                        <p class="bio">“Anafor”. Sağ kol. Abileri olmadı mı onda söz. İyi de idare ediyor. Ha bir de aşık oldu Anafor. Ama muradına eremeyecek sanırım. Yeni sezonu bekleyeceğiz artık öğrenmek için.</p>
+                        <small class="text-muted">Yedek Oyuncu</small>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+<footer>
+    <div class="copyright container">
+        &copy; Sıfır Bir 2017 Tüm Hakları Saklıdır.
+    </div>
+</footer>
+
+<script src="assets/plugins/jquery/js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
